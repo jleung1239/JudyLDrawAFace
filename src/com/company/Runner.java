32886dc042e87e0b740951d09c02dd6ed954b001
BT_1N3_13 <a>
2018-11-29 08:39:42 -0500

    a

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -5,13 +5,12 @@ public class Runner {
     public static void main(String[] args) {
 
         String [][] face = new String [5][8];
-        faceFeatures face1 = new faceFeatures(face);
+        FaceDrawing face1 = new FaceDrawing(face);
 
         face1.fill("X");
-        face1.edit(" ",0,0)
+        face1.edit(" ",0,0);
 
-	// write your code here
-
-
+        System.out.println(face1);
+        
     }
 }
